a5ad49776425f58f5426691bb2f9e8d0eaf97114
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2017-04-22 16:39:06 -0400

    Update CameraController.cs

--- MarioProject/Assets/Scripts/CameraController.cs
+++ MarioProject/Assets/Scripts/CameraController.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
 	public GameObject player;
 
 	Vector3 properCameraPositon;
