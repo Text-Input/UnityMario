dcd876ef40f645441df29769f6ef3c453ab3dfbe
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2017-04-22 16:32:54 -0400

    Fixed unwanted jittering on camera
    I moved `properCameraPositon = transform.position;` from `start()` to `lateUpdate()`. I also added comments to make the code understandable for debugging.

--- MarioProject/Assets/Scripts/CameraController.cs
+++ MarioProject/Assets/Scripts/CameraController.cs
@@ -1,39 +1,38 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
 
-    public GameObject player;
+	Vector3 properCameraPositon;
+	Vector3 playerScreenPos;
 
-    Vector3 properCameraPositon;
-    
+	float offsetX;
+	float locationY;
 
-    float offsetX;
-    float locationY;
-    Vector3 playerScreenPos;
-
-    void Start () {
-        offsetX = transform.position.x - player.transform.position.x;
-
-        properCameraPositon = transform.position;
+	void Start () {
+		offsetX = transform.position.x - player.transform.position.x;
 	}
 	
-	
 	void LateUpdate () {
+		properCameraPositon = transform.position; //I moveed the properCameraPosition set to the update. Without resetting the value each time, it would accumulate
 
-        playerScreenPos = Camera.main.WorldToScreenPoint(player.transform.position);
+		//get player position in the "real world" to a camera location
+		playerScreenPos = Camera.main.WorldToScreenPoint (player.transform.position);
 
-        if (playerScreenPos.y >= Screen.height / 2) {
-            properCameraPositon.y = properCameraPositon.y + playerScreenPos.y - (Screen.height / 2);
-        }
-
-        if (playerScreenPos.y <= (Screen.height / 10)) {
-            properCameraPositon.y = properCameraPositon.y - playerScreenPos.y - (Screen.height / 10);
-        }
-
-        locationY = Mathf.Lerp(transform.position.y, properCameraPositon.y, Time.deltaTime);
-
-        transform.position = new Vector3(offsetX + player.transform.position.x, locationY, -10);
-    }
+		if (playerScreenPos.y >= Screen.height / 2) { //player is at the correct height
+			//the camera position
+			properCameraPositon.y += playerScreenPos.y - (Screen.height / 2);
+		} else if (playerScreenPos.y <= (Screen.height / 10)) { //player is at correct height
+			//how low below the threshold is the player?
+			properCameraPositon.y += playerScreenPos.y - (Screen.height / 10); //this gives a negative number, so we don't need to subtract it
+		}
+		locationY = Mathf.Lerp (transform.position.y, properCameraPositon.y, Time.deltaTime); //interpolate y value
+		
+		transform.position = new Vector3 (offsetX + player.transform.position.x, //keep x distance
+			locationY, //set the interpolated Y value
+			transform.position.z);
+	}
 }
